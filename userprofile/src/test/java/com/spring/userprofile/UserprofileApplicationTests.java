e89fa48ac67c7bf7aface3378ed1e1f009034652
raihanuddin561 <raihanuddin561@gmail.com>
2020-01-07 21:50:54 +0600

    user profile creating to upload profile with pic

---
+++ userprofile/src/test/java/com/spring/userprofile/UserprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.userprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
